342cf4b617e190e543f2250a6496ef3563c597fc
gukt <29283212@qq.com>
2025-10-01 02:04:40 +0800

    Init Commit

---
+++ src/main/java/com/example/common/config/JpaAuditingConfig.java
@@ -0,0 +1,10 @@
+package com.example.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfig {
+    // 无需额外配置，只需启用 JPA 审计功能
+}
